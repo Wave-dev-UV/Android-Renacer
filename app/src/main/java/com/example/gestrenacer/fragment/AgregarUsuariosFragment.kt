7c84a751278d2053081b48d2437092fb1c81cfa5
jhons <104960124+SuescunJhon@users.noreply.github.com>
2024-09-28 22:19:36 -0500

    Se creo la ventana para crear un usuario

---
+++ app/src/main/java/com/example/gestrenacer/fragment/AgregarUsuariosFragment.kt
@@ -0,0 +1,30 @@
+package com.example.gestrenacer.fragment
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.gestrenacer.R
+
+class AgregarUsuariosFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AgregarUsuariosFragment()
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_agregar_usuarios, container, false)
+    }
+}
