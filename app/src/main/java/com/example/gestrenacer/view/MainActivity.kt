03446dc6cea6f315eb0ad2afed95f736de7cbf03
Cristian O. <84862634+Criser2013@users.noreply.github.com>
2024-09-30 08:58:30 -0500

    Layout inicial, sin recyclerview implementado.

--- app/src/main/java/com/example/gestrenacer/view/MainActivity.kt
+++ app/src/main/java/com/example/gestrenacer/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.gestrenacer
+package com.example.gestrenacer.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.gestrenacer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
