aeed01f541c720486aa322f138162d7dda6ed9f1
jhons <104960124+SuescunJhon@users.noreply.github.com>
2024-10-01 22:23:39 -0500

    Cambio en el modelo de datos

--- app/src/main/java/com/example/gestrenacer/models/User.kt
+++ app/src/main/java/com/example/gestrenacer/models/User.kt
@@ -3,21 +3,21 @@ package com.example.gestrenacer.models
 import java.io.Serializable
 
 data class User(
-    var nombre: String?,
-    var apellido: String?,
-    var id: String?,
-    var tipoId: String?,
-    var celular: String?,
-    var direccion: String?,
-    var eps: String?,
-    var nombreContacto: String?,
-    var celularContacto: String?,
-    var parentescoContacto: String?,
-    var direccionContacto: String?,
-    var esLider: Boolean?,
-    var tieneAcceso: Boolean?
+    var nombre: String,
+    var apellido: String,
+    var id: String,
+    var tipoId: String,
+    var celular: String,
+    var direccion: String,
+    var eps: String,
+    var nombreContacto: String,
+    var celularContacto: String,
+    var parentescoContacto: String,
+    var direccionContacto: String,
+    var esLider: Boolean,
+    var tieneAcceso: Boolean
 ): Serializable {
-    constructor(): this(null, null, null, null, null, null,
-        null, null, null, null, null,
-        null, null)
+    constructor(): this("", "", "", "", "", "",
+        "", "", "", "", "",
+        false, false)
 }
