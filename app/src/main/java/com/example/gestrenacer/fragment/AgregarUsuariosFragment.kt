6b7179646659a6c16050db491c6d73f5c46735d8
jhons <104960124+SuescunJhon@users.noreply.github.com>
2024-10-01 22:08:27 -0500

    Se agrego el viewmodel y repositorio al fragment de crear usuario

--- app/src/main/java/com/example/gestrenacer/fragment/AgregarUsuariosFragment.kt
+++ app/src/main/java/com/example/gestrenacer/fragment/AgregarUsuariosFragment.kt
@@ -6,25 +6,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.gestrenacer.R
+import com.example.gestrenacer.databinding.FragmentAgregarUsuariosBinding
+import com.example.gestrenacer.models.User
+import com.example.gestrenacer.viewmodel.AppViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AgregarUsuariosFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = AgregarUsuariosFragment()
-    }
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
+    private lateinit var binding: FragmentAgregarUsuariosBinding
+    private val appViewModel: AppViewModel by viewModels()
+    private val user = User()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_agregar_usuarios, container, false)
+        binding = FragmentAgregarUsuariosBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = appViewModel
+        binding.user = user
+        controler()
+    }
+
+    private fun controler() {
     }
 }
