5b378f1f8d94c5d6af2aeaa83916e69af70f54b7
Cristian O. <84862634+Criser2013@users.noreply.github.com>
2024-10-13 16:03:11 -0500

    GPUV-12 Funcionalidad sobre los checkbox para los filtros implementados.

--- app/src/main/java/com/example/gestrenacer/view/modal/ModalBottomSheet.kt
+++ app/src/main/java/com/example/gestrenacer/view/modal/ModalBottomSheet.kt
@@ -1,27 +1,85 @@
 package com.example.gestrenacer.view.modal
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import com.example.gestrenacer.R
+import com.example.gestrenacer.databinding.SheetFiltrosBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class ModalBottomSheet : BottomSheetDialogFragment() {
+    private lateinit var binding: SheetFiltrosBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val binding = inflater.inflate(R.layout.sheet_filtros, container, false)
-        val btnCerrar = binding.findViewById<Button>(R.id.btnCerrarFiltros)
+    ): View {
+        binding = SheetFiltrosBinding.inflate(inflater)
+        return binding.root
+    }
 
-        btnCerrar.setOnClickListener {
-          dismiss()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        iniciarComponentes()
+    }
+
+    private fun iniciarComponentes(){
+        manejadoresCheckBox()
+        manejadoresRadioBtn()
+        manejadorBtnCerrar()
+        binding.btnAplicarFiltro.setOnClickListener{
+            Log.d("boton","Clicqueado")
+        }
+    }
+
+    private fun manejadoresRadioBtn(){
+        val list = listOf(binding.radioBtnEdadAsc, binding.radioBtnEdadDesc,
+            binding.radioBtnAlfabeticoAsc, binding.radioBtnAlfabeticoDesc)
+
+        for (i in list) {
+            i.setOnClickListener {
+                var text = ""
+                when(i.id) {
+                    R.id.radioBtnEdadAsc -> text=getString(R.string.lblFiltroEdadAscen)
+                    R.id.radioBtnEdadDesc -> text=getString(R.string.lblFiltroEdadDescen)
+                    R.id.radioBtnAlfabeticoAsc -> text=getString(R.string.lblFiltroAlfabeticoAscen)
+                    R.id.radioBtnAlfabeticoDesc -> text=getString(R.string.lblFiltroAlfabeticoDescen)
+                }
+                binding.lblCriterioOrden.text = text
+            }
+        }
+    }
+
+    private fun manejadoresCheckBox(){
+        val list = listOf(binding.checkCasado, binding.checkSoltero,
+        binding.checkMasculino,binding.checkFemenino)
+
+        for (i in list) {
+            i.setOnClickListener { validarDatos() }
+        }
+    }
+
+    private fun manejadorBtnCerrar(){
+        binding.btnCerrarFiltros.setOnClickListener{
+            dismiss()
+        }
+    }
+
+    private fun validarDatos(){
+        var color = resources.getColor(R.color.onSelectedColorBotBar)
+        val listEstadoCheck = listOf(binding.checkCasado.isChecked, binding.checkSoltero.isChecked)
+        val listSexoCheck = listOf(binding.checkFemenino.isChecked, binding.checkMasculino.isChecked)
+        val activado = (listSexoCheck.reduce{acc, checkBox -> acc || checkBox } &&
+                listEstadoCheck.reduce{acc, checkBox -> acc || checkBox})
+
+        if (!activado) {
+            color = resources.getColor(R.color.btnDesactivado)
         }
 
-        return binding
+        binding.btnAplicarFiltro.setBackgroundColor(color)
+        binding.btnAplicarFiltro.isEnabled = activado
     }
 
         companion object {
