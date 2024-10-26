ae0639677496df60d6906ef7897c3a5e71c3fed5
Cristian O. <84862634+Criser2013@users.noreply.github.com>
2024-10-25 19:12:18 -0500

    Errores en navegación y acceso a ventana de ver información de un feligrés arreglados.

--- app/src/main/java/com/example/gestrenacer/view/adapter/UserAdapter.kt
+++ app/src/main/java/com/example/gestrenacer/view/adapter/UserAdapter.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.navigation.NavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gestrenacer.R
@@ -79,20 +80,22 @@ class UserAdapter(
 
             manejadorClicCard(user)
             manejadorAnadirPendientes(user)
+            desactivarBtnLlamar()
+        }
 
+        private fun desactivarBtnLlamar(){
+            if (rol !in listOf("Administrador","Gestor")){
+                binding.addPendingUser.isVisible = false
+            }
         }
 
         private fun manejadorClicCard(user: User){
-            if (rol != "Visualizador") {
-                binding.cardFeligres.setOnClickListener {
-                    val bundle = Bundle().apply {
-                        putSerializable("dataFeligres", user)
-                        putString("rol", rol)
-                    }
-                    navController.navigate(R.id.action_listarFragment_to_visualizarUsuarioFragment, bundle)
+            binding.cardFeligres.setOnClickListener {
+                val bundle = Bundle().apply {
+                    putSerializable("dataFeligres", user)
+                    putString("rol", rol)
                 }
-            } else {
-                binding.cardFeligres.setOnClickListener(null)
+                navController.navigate(R.id.action_listarFragment_to_visualizarUsuarioFragment, bundle)
             }
         }
 
