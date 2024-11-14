df8f5bf97c7275a01a01cc29e7eb0ad230e27f24
Cristian O. <84862634+Criser2013@users.noreply.github.com>
2024-11-14 15:08:53 -0500

    GPUV-17 Archivos innecesarios eliminados.

--- app/src/main/java/com/example/gestrenacer/models/PeticionDesuscribir.kt
+++
@@ -1,6 +0,0 @@
-package com.example.gestrenacer.models
-
-data class PeticionDesuscribir(
-    val arn: String,
-    val telefono: String
-)
--- app/src/main/java/com/example/gestrenacer/models/PeticionSuscribir.kt
+++
@@ -1,7 +0,0 @@
-package com.example.gestrenacer.models
-
-data class  PeticionSuscribir(
-    val telefono: String,
-    val arn: String,
-    val numero: String
-)
